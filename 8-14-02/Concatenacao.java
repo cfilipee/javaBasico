ae2b3d228de6da9ac7097dcd85b2fad1529499cc
cfilipee <caiofilipee@gmail.com>
2020-02-14 18:11:01 -0300

    Concatenação

---
+++ 8-14-02/Concatenacao.java
@@ -0,0 +1,12 @@
+public class Concatenacao{
+
+	public static void main(String[] args) {
+		
+		String primeiro = "Esse é o ";
+		String segundo = "° exemplo da ";
+		String terceiro = "vida";
+		Integer numeroUm = 1;
+
+		System.out.println(primeiro + numeroUm + segundo + terceiro);
+	}
+}
